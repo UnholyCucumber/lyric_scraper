07ee7d26c44c1ef39654384240aa6bcd8feca1ac
ericdan8 <eric.dan8@gmail.com>
2019-05-12 13:39:53 -0400

    Analyze each line and also song as a whole

--- TestScrape.py
+++ TestScrape.py
@@ -1,12 +1,21 @@
 import lyricsgenius
-import vaderSentiment
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 genius = lyricsgenius.Genius("PcLPErAXnHEREVUwt1joZmXNyW-pChC-K7OAZdWm7WPxMWaC0ui9fEFgKV6W9SBF")
 # artist = genius.search_artist("Andy Shauf", max_songs=3, sort="title")
 
-song = genius.search_song("Backseat Freestyle", "Kendrick Lamar")
-print(song.lyrics)
+song = genius.search_song("Happy", "Pharrell Williams")
+lyrics = song.lyrics
+def isWrittenLine(line) :
+    return line and not line.startswith('[')
+to_anlayze = list(filter(isWrittenLine, lyrics.splitlines()))
+print(to_anlayze)
 
+analyzer = SentimentIntensityAnalyzer()
+for line in to_anlayze:
+    scores = analyzer.polarity_scores(line)
+    print("{:-<65} {}".format(line, str(scores)))
 
-
-# print(artist.songs)
+full_lyrics = ' '.join(to_anlayze)
+vs = analyzer.polarity_scores(full_lyrics)
+print("{:-<65} {}".format(full_lyrics, str(vs)))
