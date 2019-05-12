f370712d0b0ccea0aede9057c5064b24c0dd170c
ericdan8 <eric.dan8@gmail.com>
2019-05-12 13:18:54 -0400

    Add requirements and search for song

--- TestScrape.py
+++ TestScrape.py
@@ -1,4 +1,12 @@
 import lyricsgenius
+import vaderSentiment
+
 genius = lyricsgenius.Genius("PcLPErAXnHEREVUwt1joZmXNyW-pChC-K7OAZdWm7WPxMWaC0ui9fEFgKV6W9SBF")
-artist = genius.search_artist("Andy Shauf", max_songs=3, sort="title")
-print(artist.songs)
+# artist = genius.search_artist("Andy Shauf", max_songs=3, sort="title")
+
+song = genius.search_song("Backseat Freestyle", "Kendrick Lamar")
+print(song.lyrics)
+
+
+
+# print(artist.songs)
